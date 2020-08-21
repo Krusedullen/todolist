e1a5d4712dd4987c949020095701c716972383b4
ThereseSB <thereseskaar@hotmail.com>
2020-08-21 10:50:55 +0200

    add empty ejes template file

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
+
+app.use("view engine", "ejs");
 
 app.get("/", function(req, res){
   var today = new Date();
   var currentDay = today.getDay();
   if( currentDay === 6||currentDay === 0){
     res.send("Yay it is the weekend!");
-  } else { res.send("it is a workday :/");}
+  } else { res.sendFile(__dirname + "/index.html");}
 });
 
 app.listen(3000, function(){
